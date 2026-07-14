24208e59bc386be7944e6d27fdd442a39bc7cc59
Alya Karim <A00268252@student.tus.ie>
2026-07-14 18:12:39 +0100

    updated changes

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -1,6 +1,4 @@
 from sqlalchemy.orm import Session
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 from . import models, schemas
 
 '''create_task()  → add a task
@@ -9,26 +7,13 @@ get_task()     → get one task by id
 update_task()  → edit a task
 delete_task()  → remove a task'''
 
-//create
-
-def create_task(db: Session, task: schemas.TaskCreate):
-    db_task = models.Task(**task.model_dump())
-=======
-=======
->>>>>>> Stashed changes
-
-from . import models, schemas
-
+#create
 
 def create_task(db: Session, task: schemas.TaskCreate):
     db_task = models.Task(
         title=task.title,
         note=task.note,
     )
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
 
     db.add(db_task)
     db.commit()
@@ -36,45 +21,28 @@ def create_task(db: Session, task: schemas.TaskCreate):
 
     return db_task
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-//read
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
+#read
 def get_tasks(db: Session):
     return db.query(models.Task).all()
 
 
 def get_task(db: Session, task_id: int):
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     return db.query(models.Task).filter(models.Task.id == task_id).first()
 
-//update
+#update
 
 def update_task(db: Session, task_id: int, task_update: schemas.TaskUpdate):
-=======
-=======
->>>>>>> Stashed changes
     return (
         db.query(models.Task)
         .filter(models.Task.id == task_id)
         .first()
     )
 
-
 def update_task(
     db: Session,
     task_id: int,
     task_update: schemas.TaskUpdate,
 ):
-<<<<<<< Updated upstream
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
     db_task = get_task(db, task_id)
 
     if db_task is None:
@@ -90,14 +58,7 @@ def update_task(
 
     return db_task
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-//delete
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
-
+#delete
 def delete_task(db: Session, task_id: int):
     db_task = get_task(db, task_id)
 
